592aaf78cb1b2c43e4e55d310540b4b3adc59b15
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-10-18 20:06:37 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@70ffcb50884d1b0a7658207a2db794801f9a8baf 🚀

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['is_5fdate_0',['is_date',['../namespaceritsuko.html#af53ad376d532eaac211b7cd93f3dda4c',1,'ritsuko']]],
   ['is_5fdate_5fprefix_1',['is_date_prefix',['../namespaceritsuko.html#a0dbbf9102b54e6f3f2f11bd0bdb721d8',1,'ritsuko']]],
-  ['is_5frfc3339_2',['is_rfc3339',['../namespaceritsuko.html#abf542db46552ae71530ecfccd1f66505',1,'ritsuko']]],
-  ['is_5frfc3339_5fsuffix_3',['is_rfc3339_suffix',['../namespaceritsuko.html#ac5f8ff07715c8177a709e440eb029f30',1,'ritsuko']]],
-  ['iterate_5f1d_5fblocks_4',['iterate_1d_blocks',['../namespaceritsuko_1_1hdf5.html#a0cdd51a078df2aecc261499ae5506595',1,'ritsuko::hdf5']]],
-  ['iterate_5f1d_5fblocks_2ehpp_5',['iterate_1d_blocks.hpp',['../iterate__1d__blocks_8hpp.html',1,'']]]
+  ['is_5fdate_5ftime_2ehpp_2',['is_date_time.hpp',['../is__date__time_8hpp.html',1,'']]],
+  ['is_5frfc3339_3',['is_rfc3339',['../namespaceritsuko.html#abf542db46552ae71530ecfccd1f66505',1,'ritsuko']]],
+  ['is_5frfc3339_5fsuffix_4',['is_rfc3339_suffix',['../namespaceritsuko.html#ac5f8ff07715c8177a709e440eb029f30',1,'ritsuko']]],
+  ['iterate_5f1d_5fblocks_5',['iterate_1d_blocks',['../namespaceritsuko_1_1hdf5.html#a0cdd51a078df2aecc261499ae5506595',1,'ritsuko::hdf5']]],
+  ['iterate_5f1d_5fblocks_2ehpp_6',['iterate_1d_blocks.hpp',['../iterate__1d__blocks_8hpp.html',1,'']]]
 ];
--- docs/search/files_3.js
+++ docs/search/files_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['iterate_5f1d_5fblocks_2ehpp_0',['iterate_1d_blocks.hpp',['../iterate__1d__blocks_8hpp.html',1,'']]]
+  ['is_5fdate_5ftime_2ehpp_0',['is_date_time.hpp',['../is__date__time_8hpp.html',1,'']]],
+  ['iterate_5f1d_5fblocks_2ehpp_1',['iterate_1d_blocks.hpp',['../iterate__1d__blocks_8hpp.html',1,'']]]
 ];
