857e5c5f2720e218115137ef47f7c2ca13235e43
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-02 17:39:08 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@66a0326756cbca1690fe05823ec98722b5e48fd4 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['c_2b_2b_20utilities_20for_20parsing_20and_20validation_0',['C++ utilities for parsing and validation',['../index.html',1,'']]],
-  ['choose_5fmissing_5ffloat_5fplaceholder_1',['choose_missing_float_placeholder',['../namespaceritsuko.html#aeaa67d8a37f9aa436d6cd649f0d9b752',1,'ritsuko']]],
+  ['choose_5fmissing_5ffloat_5fplaceholder_1',['choose_missing_float_placeholder',['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko']]],
   ['choose_5fmissing_5finteger_5fplaceholder_2',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko']]],
   ['choose_5fmissing_5fplaceholder_2ehpp_3',['choose_missing_placeholder.hpp',['../choose__missing__placeholder_8hpp.html',1,'']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['choose_5fmissing_5ffloat_5fplaceholder_0',['choose_missing_float_placeholder',['../namespaceritsuko.html#aeaa67d8a37f9aa436d6cd649f0d9b752',1,'ritsuko']]],
+  ['choose_5fmissing_5ffloat_5fplaceholder_0',['choose_missing_float_placeholder',['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko']]],
   ['choose_5fmissing_5finteger_5fplaceholder_1',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko']]]
 ];
