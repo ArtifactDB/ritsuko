490cd7c3423adcbb85af77517581788d9c8d90b1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-03 06:27:54 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@7dca640d6c9c4342f83b6f21dad13be1dc19bfc5 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -9,6 +9,8 @@ var searchData=
   ['get_5fmissing_5fplaceholder_5fattribute_2ehpp_6',['get_missing_placeholder_attribute.hpp',['../get__missing__placeholder__attribute_8hpp.html',1,'']]],
   ['get_5fname_7',['get_name',['../namespaceritsuko_1_1hdf5.html#a199d4e8292ebb954ab9f505764ebf2d9',1,'ritsuko::hdf5']]],
   ['get_5fname_2ehpp_8',['get_name.hpp',['../get__name_8hpp.html',1,'']]],
-  ['get_5fscalar_5fdataset_9',['get_scalar_dataset',['../namespaceritsuko_1_1hdf5.html#a60bb02dcde8b7de2048930cdfd7541d0',1,'ritsuko::hdf5']]],
-  ['gt_10',['gt',['../structritsuko_1_1Version.html#a957cd60b32aa1a42f8590355ba23b4d2',1,'ritsuko::Version']]]
+  ['get_5fscalar_5fattribute_9',['get_scalar_attribute',['../namespaceritsuko_1_1hdf5.html#a363a6effad1f378fea2e0133c48965af',1,'ritsuko::hdf5']]],
+  ['get_5fscalar_5fattribute_2ehpp_10',['get_scalar_attribute.hpp',['../get__scalar__attribute_8hpp.html',1,'']]],
+  ['get_5fscalar_5fdataset_11',['get_scalar_dataset',['../namespaceritsuko_1_1hdf5.html#a60bb02dcde8b7de2048930cdfd7541d0',1,'ritsuko::hdf5']]],
+  ['gt_12',['gt',['../structritsuko_1_1Version.html#a957cd60b32aa1a42f8590355ba23b4d2',1,'ritsuko::Version']]]
 ];
--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -5,6 +5,7 @@ var searchData=
   ['get_5fdataset_2',['get_dataset',['../namespaceritsuko_1_1hdf5.html#a8a30dedd3c6544a1745035581cd2c31d',1,'ritsuko::hdf5']]],
   ['get_5fmissing_5fplaceholder_5fattribute_3',['get_missing_placeholder_attribute',['../namespaceritsuko_1_1hdf5.html#a4c441c7a6e4745df9a604045799bdbd2',1,'ritsuko::hdf5']]],
   ['get_5fname_4',['get_name',['../namespaceritsuko_1_1hdf5.html#a199d4e8292ebb954ab9f505764ebf2d9',1,'ritsuko::hdf5']]],
-  ['get_5fscalar_5fdataset_5',['get_scalar_dataset',['../namespaceritsuko_1_1hdf5.html#a60bb02dcde8b7de2048930cdfd7541d0',1,'ritsuko::hdf5']]],
-  ['gt_6',['gt',['../structritsuko_1_1Version.html#a957cd60b32aa1a42f8590355ba23b4d2',1,'ritsuko::Version']]]
+  ['get_5fscalar_5fattribute_5',['get_scalar_attribute',['../namespaceritsuko_1_1hdf5.html#a363a6effad1f378fea2e0133c48965af',1,'ritsuko::hdf5']]],
+  ['get_5fscalar_5fdataset_6',['get_scalar_dataset',['../namespaceritsuko_1_1hdf5.html#a60bb02dcde8b7de2048930cdfd7541d0',1,'ritsuko::hdf5']]],
+  ['gt_7',['gt',['../structritsuko_1_1Version.html#a957cd60b32aa1a42f8590355ba23b4d2',1,'ritsuko::Version']]]
 ];
