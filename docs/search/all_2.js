77c964a156d7a96efbeb059a4b94b667fb8eb3da
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-03 06:14:46 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@63fc9ab90c8dbcdd858b41ef32a23fb99a91bc11 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['eq_0',['eq',['../structritsuko_1_1Version.html#a122b1410448ee5e72535de906ae4a652',1,'ritsuko::Version']]],
-  ['exceeds_5ffloat_5flimit_1',['exceeds_float_limit',['../namespaceritsuko_1_1hdf5.html#acaeb27fa68cb7564cfdec29e4b6d6789',1,'ritsuko::hdf5']]],
-  ['exceeds_5finteger_5flimit_2',['exceeds_integer_limit',['../namespaceritsuko_1_1hdf5.html#a8101d57fbdf4a10b2297509acb1c2c0f',1,'ritsuko::hdf5']]],
+  ['exceeds_5ffloat_5flimit_1',['exceeds_float_limit',['../namespaceritsuko_1_1hdf5.html#acaeb27fa68cb7564cfdec29e4b6d6789',1,'ritsuko::hdf5::exceeds_float_limit(const H5::DataSet &amp;handle, size_t precision)'],['../namespaceritsuko_1_1hdf5.html#a81ca0022b59f5b2922d9899af3f3ec07',1,'ritsuko::hdf5::exceeds_float_limit(const H5::Attribute &amp;handle, size_t precision)']]],
+  ['exceeds_5finteger_5flimit_2',['exceeds_integer_limit',['../namespaceritsuko_1_1hdf5.html#a6cd65a78811faa2894a672f5ce993949',1,'ritsuko::hdf5::exceeds_integer_limit(const H5::IntType &amp;itype, size_t precision, bool is_signed)'],['../namespaceritsuko_1_1hdf5.html#a8101d57fbdf4a10b2297509acb1c2c0f',1,'ritsuko::hdf5::exceeds_integer_limit(const H5::DataSet &amp;handle, size_t precision, bool is_signed)'],['../namespaceritsuko_1_1hdf5.html#ac47306af2cffbd1c5e6510e263433917',1,'ritsuko::hdf5::exceeds_integer_limit(const H5::Attribute &amp;handle, size_t precision, bool is_signed)']]],
   ['exceeds_5flimit_2ehpp_3',['exceeds_limit.hpp',['../exceeds__limit_8hpp.html',1,'']]]
 ];
