3e4c5dd8520da16c69e1e990d4bdb1a55788e322
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-06 19:59:59 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@b1bc4df1806215ec52881122657eef85cd003405 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['c_2b_2b_20utilities_20for_20parsing_20and_20validation_0',['C++ utilities for parsing and validation',['../index.html',1,'']]],
-  ['choose_5fmissing_5ffloat_5fplaceholder_1',['choose_missing_float_placeholder',['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko']]],
-  ['choose_5fmissing_5finteger_5fplaceholder_2',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko']]],
+  ['choose_5fmissing_5ffloat_5fplaceholder_1',['choose_missing_float_placeholder',['../namespaceritsuko.html#a8f0d04408fd63a41f12337d8e27b20f9',1,'ritsuko::choose_missing_float_placeholder(Iterator start, Iterator end, Mask mask, bool skip_nan)'],['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko::choose_missing_float_placeholder(Iterator start, Iterator end, bool skip_nan=false)']]],
+  ['choose_5fmissing_5finteger_5fplaceholder_2',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a1e2efff2630c08b7145f28b6dd0caaac',1,'ritsuko::choose_missing_integer_placeholder(Iterator start, Iterator end, Mask mask)'],['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko::choose_missing_integer_placeholder(Iterator start, Iterator end)']]],
   ['choose_5fmissing_5fplaceholder_2ehpp_3',['choose_missing_placeholder.hpp',['../choose__missing__placeholder_8hpp.html',1,'']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['choose_5fmissing_5ffloat_5fplaceholder_0',['choose_missing_float_placeholder',['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko']]],
-  ['choose_5fmissing_5finteger_5fplaceholder_1',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko']]]
+  ['choose_5fmissing_5ffloat_5fplaceholder_0',['choose_missing_float_placeholder',['../namespaceritsuko.html#a8f0d04408fd63a41f12337d8e27b20f9',1,'ritsuko::choose_missing_float_placeholder(Iterator start, Iterator end, Mask mask, bool skip_nan)'],['../namespaceritsuko.html#a6039e4e9382e5e3c5ccab69df2a277ba',1,'ritsuko::choose_missing_float_placeholder(Iterator start, Iterator end, bool skip_nan=false)']]],
+  ['choose_5fmissing_5finteger_5fplaceholder_1',['choose_missing_integer_placeholder',['../namespaceritsuko.html#a1e2efff2630c08b7145f28b6dd0caaac',1,'ritsuko::choose_missing_integer_placeholder(Iterator start, Iterator end, Mask mask)'],['../namespaceritsuko.html#a50a9e4b90108738c8c487c4346d63e61',1,'ritsuko::choose_missing_integer_placeholder(Iterator start, Iterator end)']]]
 ];
