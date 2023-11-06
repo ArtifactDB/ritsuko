8367c5bb6130e61f7000f8463177afcf7240c7e2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-06 11:58:14 -0800

    Added a masking overload for choosing the missing placeholder.
    This is necessary for frameworks like Python where the missing values
    are specified via a separate masking vector.

--- tests/src/choose_missing_placeholder.cpp
+++ tests/src/choose_missing_placeholder.cpp
@@ -72,6 +72,51 @@ TEST(ChooseMissingPlaceholder, UnsignedInteger) {
     }
 }
 
+TEST(ChooseMissingPlaceholder, IntegerMask) {
+    std::vector<int32_t> foo { 1, 2, 3 };
+    std::vector<char> mask(foo.size());
+    {
+        auto found = ritsuko::choose_missing_integer_placeholder(foo.begin(), foo.end(), mask.begin());
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, -2147483648);
+    }
+
+    foo.push_back(-2147483648);
+    mask.push_back(0);
+    {
+        auto found = ritsuko::choose_missing_integer_placeholder(foo.begin(), foo.end(), mask.begin());
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, 2147483647);
+    }
+
+    mask.back() = 1;
+    {
+        auto found = ritsuko::choose_missing_integer_placeholder(foo.begin(), foo.end(), mask.begin());
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, -2147483648);
+    }
+
+    mask.back() = 0;
+    foo.push_back(2147483647);
+    foo.push_back(0);
+    foo.push_back(-2147483647);
+    mask.push_back(0);
+    mask.push_back(0);
+    mask.push_back(1);
+    {
+        auto found = ritsuko::choose_missing_integer_placeholder(foo.begin(), foo.end(), mask.begin());
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, -2147483647);
+    }
+
+    mask.back() = 0;
+    {
+        auto found = ritsuko::choose_missing_integer_placeholder(foo.begin(), foo.end(), mask.begin());
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, -2147483646);
+    }
+}
+
 TEST(ChooseMissingPlaceholder, Float) {
     std::vector<double> foo { 1, 2, 3 };
 
@@ -134,5 +179,60 @@ TEST(ChooseMissingPlaceholder, Float) {
         auto found = ritsuko::choose_missing_float_placeholder(foo.begin(), foo.end());
         EXPECT_TRUE(found.first);
         EXPECT_EQ(found.second, lowest / 2);
+    }
+}
+
+TEST(ChooseMissingPlaceholder, FloatMask) {
+    std::vector<double> foo { 1, 2, 3 };
+    std::vector<char> mask(foo.size());
+
+    if constexpr(std::numeric_limits<double>::is_iec559) {
+        auto nan = std::numeric_limits<double>::quiet_NaN();
+        auto pinf = std::numeric_limits<double>::infinity();
+        auto ninf = -std::numeric_limits<double>::infinity();
+
+        {
+            auto found = ritsuko::choose_missing_float_placeholder(foo.begin(), foo.end(), mask.begin(), false);
+            EXPECT_TRUE(found.first);
+            EXPECT_TRUE(std::isnan(found.second));
+        }
+        foo.push_back(nan);
+        mask.push_back(1);
+
+        {
+            auto found = ritsuko::choose_missing_float_placeholder(foo.begin(), foo.end(), mask.begin(), false);
+            EXPECT_TRUE(found.first);
+            EXPECT_TRUE(std::isnan(found.second));
+        }
+        mask.back() = 0;
+        foo.push_back(pinf);
+        foo.push_back(ninf);
+        mask.push_back(0);
+        mask.push_back(0);
+    }
+
+    auto max = std::numeric_limits<double>::max();
+    auto lowest = std::numeric_limits<double>::lowest();
+    foo.push_back(max);
+    foo.push_back(lowest);
+    foo.push_back(0);
+    mask.push_back(0);
+    mask.push_back(0);
+    mask.push_back(0);
+
+    foo.push_back(lowest/2);
+    mask.push_back(1);
+    {
+        auto found = ritsuko::choose_missing_float_placeholder(foo.begin(), foo.end(), mask.begin(), false);
+        EXPECT_TRUE(found.first);
+        EXPECT_EQ(found.second, lowest / 2);
+    }
+
+    mask.back() = 0;
+    {
+        auto found = ritsuko::choose_missing_float_placeholder(foo.begin(), foo.end(), mask.begin(), false);
+        EXPECT_TRUE(found.first);
+        EXPECT_TRUE(found.second < lowest / 2);
+        EXPECT_TRUE(found.second > lowest);
     }
 }
