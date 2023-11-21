a09cebfc89f6b4adcf5bd5dc7f6adf4f04c00624
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-21 16:29:49 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@1ee10e4cfd321f0149f2ed53ad81d1a36018f5b0 🚀

--- docs/menudata.js
+++ docs/menudata.js
@@ -27,8 +27,26 @@ var menudata={children:[
 {text:"Namespaces",url:"namespaces.html",children:[
 {text:"Namespace List",url:"namespaces.html"},
 {text:"Namespace Members",url:"namespacemembers.html",children:[
-{text:"All",url:"namespacemembers.html"},
-{text:"Functions",url:"namespacemembers_func.html"}]}]},
+{text:"All",url:"namespacemembers.html",children:[
+{text:"a",url:"namespacemembers.html#index_a"},
+{text:"c",url:"namespacemembers.html#index_c"},
+{text:"e",url:"namespacemembers.html#index_e"},
+{text:"g",url:"namespacemembers.html#index_g"},
+{text:"i",url:"namespacemembers.html#index_i"},
+{text:"l",url:"namespacemembers.html#index_l"},
+{text:"o",url:"namespacemembers.html#index_o"},
+{text:"p",url:"namespacemembers.html#index_p"},
+{text:"r",url:"namespacemembers.html#index_r"}]},
+{text:"Functions",url:"namespacemembers_func.html",children:[
+{text:"a",url:"namespacemembers_func.html#index_a"},
+{text:"c",url:"namespacemembers_func.html#index_c"},
+{text:"e",url:"namespacemembers_func.html#index_e"},
+{text:"g",url:"namespacemembers_func.html#index_g"},
+{text:"i",url:"namespacemembers_func.html#index_i"},
+{text:"l",url:"namespacemembers_func.html#index_l"},
+{text:"o",url:"namespacemembers_func.html#index_o"},
+{text:"p",url:"namespacemembers_func.html#index_p"},
+{text:"r",url:"namespacemembers_func.html#index_r"}]}]}]},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['le_0',['le',['../structritsuko_1_1Version.html#aad9ea246db4488b25dc147cd2c1fc587',1,'ritsuko::Version']]],
-  ['load_5f1d_5fstring_5fattribute_1',['load_1d_string_attribute',['../namespaceritsuko_1_1hdf5.html#add2dd33e65a9809d1d616e5dd4a64e81',1,'ritsuko::hdf5']]],
-  ['load_5f1d_5fstring_5fdataset_2',['load_1d_string_dataset',['../namespaceritsuko_1_1hdf5.html#ae1d98f3668942a1f74f3c22e515beaf1',1,'ritsuko::hdf5']]],
-  ['load_5fscalar_5fstring_5fattribute_3',['load_scalar_string_attribute',['../namespaceritsuko_1_1hdf5.html#ae09b2aaf282ec0809b610528f4bf87b3',1,'ritsuko::hdf5::load_scalar_string_attribute(const H5::Attribute &amp;attr)'],['../namespaceritsuko_1_1hdf5.html#a160a83252d5d8ef5b886acf9726fa720',1,'ritsuko::hdf5::load_scalar_string_attribute(const Object_ &amp;handle, const char *field)']]],
-  ['lt_4',['lt',['../structritsuko_1_1Version.html#a60e71e8fd3daee1ed661bdca1ff8f346',1,'ritsuko::Version']]]
+  ['load_5f1d_5fnumeric_5fattribute_1',['load_1d_numeric_attribute',['../namespaceritsuko_1_1hdf5.html#aee36514dd3190123248046c4842e3b2b',1,'ritsuko::hdf5']]],
+  ['load_5f1d_5fnumeric_5fdataset_2',['load_1d_numeric_dataset',['../namespaceritsuko_1_1hdf5.html#ade5f3ba04fb900893f7cd9e4938c9185',1,'ritsuko::hdf5']]],
+  ['load_5f1d_5fstring_5fattribute_3',['load_1d_string_attribute',['../namespaceritsuko_1_1hdf5.html#add2dd33e65a9809d1d616e5dd4a64e81',1,'ritsuko::hdf5']]],
+  ['load_5f1d_5fstring_5fdataset_4',['load_1d_string_dataset',['../namespaceritsuko_1_1hdf5.html#ae1d98f3668942a1f74f3c22e515beaf1',1,'ritsuko::hdf5']]],
+  ['load_5fscalar_5fstring_5fattribute_5',['load_scalar_string_attribute',['../namespaceritsuko_1_1hdf5.html#ae09b2aaf282ec0809b610528f4bf87b3',1,'ritsuko::hdf5::load_scalar_string_attribute(const H5::Attribute &amp;attr)'],['../namespaceritsuko_1_1hdf5.html#a160a83252d5d8ef5b886acf9726fa720',1,'ritsuko::hdf5::load_scalar_string_attribute(const Object_ &amp;handle, const char *field)']]],
+  ['lt_6',['lt',['../structritsuko_1_1Version.html#a60e71e8fd3daee1ed661bdca1ff8f346',1,'ritsuko::Version']]]
 ];
