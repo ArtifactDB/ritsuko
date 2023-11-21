ae4bae803da49b08838fe79f49e0fbebaf28c6c8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-21 07:30:34 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@c97e5f55e232c84eb160f1d488a6f12812cd990e 🚀

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['choose_5fmissing_5fplaceholder_2ehpp_0',['choose_missing_placeholder.hpp',['../choose__missing__placeholder_8hpp.html',1,'']]]
+  ['choose_5fdatatype_2ehpp_0',['choose_datatype.hpp',['../choose__datatype_8hpp.html',1,'']]],
+  ['choose_5fmissing_5fplaceholder_2ehpp_1',['choose_missing_placeholder.hpp',['../choose__missing__placeholder_8hpp.html',1,'']]]
 ];
