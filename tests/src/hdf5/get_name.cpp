aecf280a283347b112ba7159115a3c44e6cdd0aa
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-22 07:53:19 -0800

    Enable convenient extraction of attribute name via get_name.

--- tests/src/hdf5/get_name.cpp
+++ tests/src/hdf5/get_name.cpp
@@ -14,12 +14,18 @@ TEST(Hdf5GetName, Basic) {
         H5::H5File handle(path, H5F_ACC_TRUNC);
         handle.createDataSet("foobar", H5::PredType::NATIVE_INT, H5S_SCALAR);
         auto ghandle = handle.createGroup("whee");
-        ghandle.createGroup("stuff");
+        auto xhandle = ghandle.createGroup("stuff");
+        xhandle.createAttribute("blahblah", H5::PredType::NATIVE_INT, H5S_SCALAR);
     }
     {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         EXPECT_EQ("/foobar", ritsuko::hdf5::get_name(handle.openDataSet("foobar")));
         EXPECT_EQ("/whee", ritsuko::hdf5::get_name(handle.openGroup("whee")));
-        EXPECT_EQ("/whee/stuff", ritsuko::hdf5::get_name(handle.openGroup("whee/stuff")));
+
+        auto xhandle = handle.openGroup("whee/stuff");
+        EXPECT_EQ("/whee/stuff", ritsuko::hdf5::get_name(xhandle));
+
+        auto ahandle = xhandle.openAttribute("blahblah");
+        EXPECT_EQ("blahblah", ritsuko::hdf5::get_name(ahandle));
     }
 }
