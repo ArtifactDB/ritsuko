1e634d9f97292e635e0c5d82527de4e6460383af
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-08 07:23:19 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@b914a02fc0f8eeb5c2eab031fa49c86fcab90385 🚀

--- docs/search/files_5.js
+++ docs/search/files_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['is_5fdate_5ftime_2ehpp_0',['is_date_time.hpp',['../is__date__time_8hpp.html',1,'']]]
+  ['is_5fdate_5ftime_2ehpp_0',['is_date_time.hpp',['../is__date__time_8hpp.html',1,'']]],
+  ['is_5futf8_5fstring_2ehpp_1',['is_utf8_string.hpp',['../is__utf8__string_8hpp.html',1,'']]]
 ];
