cdaeb259b8b10f4a1a5c7266d17015d9dde86a3b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-07 22:59:22 -0800

    Extend the as_numeric_datatype to the non-fixed types.
    This flexibility is occasionally useful for more general code that
    doesn't have fixed-width expectations.

--- tests/src/hdf5/as_numeric_datatype.cpp
+++ tests/src/hdf5/as_numeric_datatype.cpp
@@ -2,7 +2,7 @@
 #include <gmock/gmock.h>
 #include "ritsuko/hdf5/as_numeric_datatype.hpp"
 
-TEST(ChooseDatatype, UnsignedIntegers) {
+TEST(ChooseDatatype, UnsignedFixedIntegers) {
     {
         auto x = ritsuko::hdf5::as_numeric_datatype<uint8_t>();
         EXPECT_EQ(x.getClass(), H5T_INTEGER);
@@ -36,7 +36,7 @@ TEST(ChooseDatatype, UnsignedIntegers) {
     }
 }
 
-TEST(ChooseDatatype, SignedIntegers) {
+TEST(ChooseDatatype, SignedFixedIntegers) {
     {
         auto x = ritsuko::hdf5::as_numeric_datatype<int8_t>();
         EXPECT_EQ(x.getClass(), H5T_INTEGER);
@@ -68,6 +68,97 @@ TEST(ChooseDatatype, SignedIntegers) {
         EXPECT_EQ(y.getSign(), H5T_SGN_2);
         EXPECT_EQ(y.getPrecision(), 64);
     }
+}
+
+TEST(ChooseDatatype, UnsignedNonFixedIntegers) {
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<unsigned char>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_NONE);
+        EXPECT_EQ(y.getPrecision(), sizeof(unsigned char) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<unsigned short>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_NONE);
+        EXPECT_EQ(y.getPrecision(), sizeof(unsigned short) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<unsigned int>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_NONE);
+        EXPECT_EQ(y.getPrecision(), sizeof(unsigned int) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<unsigned long>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_NONE);
+        EXPECT_EQ(y.getPrecision(), sizeof(unsigned long) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<unsigned long long>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_NONE);
+        EXPECT_EQ(y.getPrecision(), sizeof(unsigned long long) * 8);
+    }
+}
+
+TEST(ChooseDatatype, SignedNonFixedIntegers) {
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<signed char>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_2);
+        EXPECT_EQ(y.getPrecision(), sizeof(char) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<short>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_2);
+        EXPECT_EQ(y.getPrecision(), sizeof(short) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<int>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_2);
+        EXPECT_EQ(y.getPrecision(), sizeof(int) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<long>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_2);
+        EXPECT_EQ(y.getPrecision(), sizeof(long) * 8);
+    }
+
+    {
+        auto x = ritsuko::hdf5::as_numeric_datatype<long long>();
+        EXPECT_EQ(x.getClass(), H5T_INTEGER);
+        H5::IntType y(x);
+        EXPECT_EQ(y.getSign(), H5T_SGN_2);
+        EXPECT_EQ(y.getPrecision(), sizeof(long long) * 8);
+    }
+}
+
+TEST(ChooseDatatype, Char) {
+    auto x = ritsuko::hdf5::as_numeric_datatype<char>();
+    EXPECT_EQ(x.getClass(), H5T_INTEGER);
+    H5::IntType y(x);
+    EXPECT_EQ(y.getPrecision(), sizeof(char) * 8);
 }
 
 TEST(ChooseDatatype, Floats) {
