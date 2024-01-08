3a273b9defad0e8bf5176cda89db252537d4ec55
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-08 07:01:50 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@a03963a5cf1eadb830867f5e0e9a3b53d4f2ffd9 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['are_5ffloats_5fidentical_0',['are_floats_identical',['../namespaceritsuko.html#a74c2618a51291704bc7024b1dfc178d8',1,'ritsuko']]],
-  ['as_5fnumeric_5fdatatype_1',['as_numeric_datatype',['../namespaceritsuko_1_1hdf5.html#af22fcb7f30e2d00aa15c2da1ab9ed63a',1,'ritsuko::hdf5']]],
+  ['as_5fnumeric_5fdatatype_1',['as_numeric_datatype',['../namespaceritsuko_1_1hdf5.html#a51dedc84e7992f1ae6a6981a83c4e8b8',1,'ritsuko::hdf5']]],
   ['as_5fnumeric_5fdatatype_2ehpp_2',['as_numeric_datatype.hpp',['../as__numeric__datatype_8hpp.html',1,'']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['are_5ffloats_5fidentical_0',['are_floats_identical',['../namespaceritsuko.html#a74c2618a51291704bc7024b1dfc178d8',1,'ritsuko']]],
-  ['as_5fnumeric_5fdatatype_1',['as_numeric_datatype',['../namespaceritsuko_1_1hdf5.html#af22fcb7f30e2d00aa15c2da1ab9ed63a',1,'ritsuko::hdf5']]]
+  ['as_5fnumeric_5fdatatype_1',['as_numeric_datatype',['../namespaceritsuko_1_1hdf5.html#a51dedc84e7992f1ae6a6981a83c4e8b8',1,'ritsuko::hdf5']]]
 ];
