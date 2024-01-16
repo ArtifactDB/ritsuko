4950c01e9f4abc89f3cde5a56cb92ad5af53f8b3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-16 11:33:03 -0800

    Streamlined tests for the pick_1d_block_size.

--- tests/src/hdf5/pick_1d_block_size.cpp
+++ tests/src/hdf5/pick_1d_block_size.cpp
@@ -3,77 +3,56 @@
 #include "ritsuko/hdf5/pick_1d_block_size.hpp"
 
 TEST(Hdf5Pick1dBlockSize, Compressed) {
-    const char* path = "TEST-blocks.h5";
     hsize_t buffer = 10000;
 
     // Rounds down to the nearest multiple.
     hsize_t len = 30000;
     hsize_t chunk = 57;
+
     {
-        H5::H5File handle(path, H5F_ACC_TRUNC);
-        H5::DataSpace dspace(1, &len);
         H5::DSetCreatPropList cplist;
         cplist.setChunk(1, &chunk);
         cplist.setDeflate(8);
-        handle.createDataSet("YAY", H5::PredType::NATIVE_UINT8, dspace, cplist);
-    }
-    {
-        H5::H5File handle(path, H5F_ACC_RDONLY);
-        auto dhandle = handle.openDataSet("YAY");
-        auto block_size = ritsuko::hdf5::pick_1d_block_size(dhandle.getCreatePlist(), len, buffer);
+        auto block_size = ritsuko::hdf5::pick_1d_block_size(cplist, len, buffer);
         EXPECT_EQ(block_size, (buffer / chunk) * chunk);
+        EXPECT_TRUE(block_size > chunk);
+        EXPECT_TRUE(block_size < len);
     }
 
     // Or uses the entire chunk.
     {
-        H5::H5File handle(path, H5F_ACC_TRUNC);
-        H5::DataSpace dspace(1, &len);
         H5::DSetCreatPropList cplist;
         hsize_t chunk = 15000;
         cplist.setChunk(1, &chunk);
         cplist.setDeflate(8);
-        handle.createDataSet("YAY", H5::PredType::NATIVE_UINT8, dspace, cplist);
-    }
-    {
-        H5::H5File handle(path, H5F_ACC_RDONLY);
-        auto dhandle = handle.openDataSet("YAY");
-        auto block_size = ritsuko::hdf5::pick_1d_block_size(dhandle.getCreatePlist(), len, buffer);
-        EXPECT_EQ(block_size, 15000);
+        auto block_size = ritsuko::hdf5::pick_1d_block_size(cplist, len, buffer);
+        EXPECT_EQ(block_size, chunk);
     }
 }
 
 TEST(Hdf5Pick1dBlockSize, Uncompressed) {
-    const char* path = "TEST-blocks.h5";
     hsize_t buffer = 10000;
-
     hsize_t len = 13232;
     {
-        H5::H5File handle(path, H5F_ACC_TRUNC);
-        H5::DataSpace dspace(1, &len);
-        handle.createDataSet("YAY", H5::PredType::NATIVE_UINT8, dspace);
-    }
-    {
-        H5::H5File handle(path, H5F_ACC_RDONLY);
-        auto dhandle = handle.openDataSet("YAY");
-        auto block_size = ritsuko::hdf5::pick_1d_block_size(dhandle.getCreatePlist(), len);
+        H5::DSetCreatPropList cplist;
+        auto block_size = ritsuko::hdf5::pick_1d_block_size(cplist, len, buffer);
         EXPECT_EQ(block_size, buffer);
     }
 }
 
 TEST(Hdf5Pick1dBlockSize, Short) {
-    const char* path = "TEST-blocks.h5";
     hsize_t buffer = 10000;
 
     hsize_t shortlen = 5;
     {
-        H5::H5File handle(path, H5F_ACC_TRUNC);
-        H5::DataSpace dspace(1, &shortlen);
-        handle.createDataSet("YAY", H5::PredType::NATIVE_UINT8, dspace);
-    }
-    {
-        H5::H5File handle(path, H5F_ACC_RDONLY);
-        auto dhandle = handle.openDataSet("YAY");
-        auto block_size = ritsuko::hdf5::pick_1d_block_size(dhandle.getCreatePlist(), shortlen, buffer);
+        H5::DSetCreatPropList cplist;
+        auto block_size = ritsuko::hdf5::pick_1d_block_size(cplist, shortlen, buffer);
         EXPECT_EQ(block_size, shortlen);
+    }
+
+    {
+        H5::DSetCreatPropList cplist;
+        auto block_size = ritsuko::hdf5::pick_1d_block_size(cplist, 0, buffer);
+        EXPECT_EQ(block_size, 0);
     }
 }
