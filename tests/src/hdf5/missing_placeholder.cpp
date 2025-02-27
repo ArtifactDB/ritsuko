1eb87413ffdde39d51f72d6f5b56a24087561b97
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-27 14:30:44 -0800

    Minor overhaul for missing placeholder utilities.
    We split the numeric and string check functions, as the string checker
    needs to do some extra work to ensure that the string is not NULL.
    
    We also switch to returning an optional<> instead of a pair<>, to better
    signal our intention when the missing placeholder is not available.

--- tests/src/hdf5/missing_placeholder.cpp
+++ tests/src/hdf5/missing_placeholder.cpp
@@ -20,8 +20,8 @@ TEST(Hdf5MissingPlaceholder, Loading) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto found = ritsuko::hdf5::open_and_load_optional_numeric_missing_placeholder<int32_t>(dhandle, "ouch");
-        EXPECT_TRUE(found.first);
-        EXPECT_EQ(found.second, 123);
+        EXPECT_TRUE(found.has_value());
+        EXPECT_EQ(found.value(), 123);
     }
 
     // Double precision.
@@ -36,8 +36,8 @@ TEST(Hdf5MissingPlaceholder, Loading) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto found = ritsuko::hdf5::open_and_load_optional_numeric_missing_placeholder<double>(dhandle, "ouch");
-        EXPECT_TRUE(found.first);
-        EXPECT_EQ(found.second, 1.5);
+        EXPECT_TRUE(found.has_value());
+        EXPECT_EQ(found.value(), 1.5);
     }
 
     // String.
@@ -52,8 +52,8 @@ TEST(Hdf5MissingPlaceholder, Loading) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto found = ritsuko::hdf5::open_and_load_optional_string_missing_placeholder(dhandle, "ouch");
-        EXPECT_TRUE(found.first);
-        EXPECT_EQ(found.second, std::string("YAY"));
+        EXPECT_TRUE(found.has_value());
+        EXPECT_EQ(found.value(), std::string("YAY"));
     }
 
     // Not even present.
@@ -64,16 +64,16 @@ TEST(Hdf5MissingPlaceholder, Loading) {
     {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
-        EXPECT_FALSE(ritsuko::hdf5::open_and_load_optional_numeric_missing_placeholder<int32_t>(dhandle, "ouch").first);
-        EXPECT_FALSE(ritsuko::hdf5::open_and_load_optional_string_missing_placeholder(dhandle, "ouch").first);
+        EXPECT_FALSE(ritsuko::hdf5::open_and_load_optional_numeric_missing_placeholder<int32_t>(dhandle, "ouch").has_value());
+        EXPECT_FALSE(ritsuko::hdf5::open_and_load_optional_string_missing_placeholder(dhandle, "ouch").has_value());
     }
 }
 
 template<typename ... Args_>
-static void expect_error(std::string msg, Args_&& ... args) {
+static void expect_numeric_error(std::string msg, Args_&& ... args) {
     EXPECT_ANY_THROW({
         try {
-            ritsuko::hdf5::check_missing_placeholder_attribute(std::forward<Args_>(args)...);
+            ritsuko::hdf5::check_numeric_missing_placeholder_attribute(std::forward<Args_>(args)...);
         } catch (std::exception& e) {
             EXPECT_THAT(e.what(), ::testing::HasSubstr(msg));
             throw;
@@ -81,7 +81,7 @@ static void expect_error(std::string msg, Args_&& ... args) {
     });
 }
 
-TEST(Hdf5MissingPlaceholder, Failed) {
+TEST(Hdf5MissingPlaceholder, CheckNumeric) {
     const char* path = "TEST-missing-placeholder.h5";
 
     // Different type.
@@ -94,9 +94,8 @@ TEST(Hdf5MissingPlaceholder, Failed) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto ahandle = dhandle.openAttribute("ouch");
-        expect_error("same type as", dhandle, ahandle, 0);
-        expect_error("same type as", dhandle, ahandle, -1);
-        ritsuko::hdf5::check_missing_placeholder_attribute(dhandle, ahandle, 1);
+        expect_numeric_error("same type as", dhandle, ahandle);
+        ritsuko::hdf5::check_numeric_missing_placeholder_attribute(dhandle, ahandle, true);
     }
 
     // Different type class.
@@ -109,24 +108,8 @@ TEST(Hdf5MissingPlaceholder, Failed) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto ahandle = dhandle.openAttribute("ouch");
-        expect_error("same type as", dhandle, ahandle, 0);
-        expect_error("same type as", dhandle, ahandle, -1);
-        expect_error("same type class as", dhandle, ahandle, 1);
-    }
-
-    // Works with strings.
-    {
-        H5::H5File handle(path, H5F_ACC_TRUNC);
-        auto dhandle = create_dataset(handle, "foobar", std::vector<std::string>{"1", "2", "3"}, true);
-        dhandle.createAttribute("ouch", H5::StrType(0, 10), H5S_SCALAR);
-    }
-    {
-        H5::H5File handle(path, H5F_ACC_RDONLY);
-        auto dhandle = handle.openDataSet("foobar");
-        auto ahandle = dhandle.openAttribute("ouch");
-        expect_error("same type as", dhandle, ahandle, 0);
-        ritsuko::hdf5::check_missing_placeholder_attribute(dhandle, ahandle, -1);
-        ritsuko::hdf5::check_missing_placeholder_attribute(dhandle, ahandle, 1);
+        expect_numeric_error("same type as", dhandle, ahandle);
+        expect_numeric_error("same type class as", dhandle, ahandle, true);
     }
 
     // Non-scalar dataset.
@@ -141,6 +124,41 @@ TEST(Hdf5MissingPlaceholder, Failed) {
         H5::H5File handle(path, H5F_ACC_RDONLY);
         auto dhandle = handle.openDataSet("foobar");
         auto ahandle = dhandle.openAttribute("ouch");
-        expect_error("scalar", dhandle, ahandle);
+        expect_numeric_error("scalar", dhandle, ahandle);
     }
+}
+
+template<typename ... Args_>
+static void expect_string_error(std::string msg, Args_&& ... args) {
+    EXPECT_ANY_THROW({
+        try {
+            ritsuko::hdf5::check_string_missing_placeholder_attribute(std::forward<Args_>(args)...);
+        } catch (std::exception& e) {
+            EXPECT_THAT(e.what(), ::testing::HasSubstr(msg));
+            throw;
+        }
+    });
+}
+
+TEST(Hdf5MissingPlaceholder, CheckString) {
+    const char* path = "TEST-missing-placeholder.h5";
+    {
+        H5::H5File handle(path, H5F_ACC_TRUNC);
+        auto dhandle = create_dataset(handle, "foobar", std::vector<std::string>{"1", "2", "3"}, true);
+        dhandle.createAttribute("ouch", H5::StrType(0, 10), H5S_SCALAR);
+        dhandle.createAttribute("pain", H5::PredType::NATIVE_INT32, H5S_SCALAR);
+        dhandle.createAttribute("stuff", H5::StrType(0, H5T_VARIABLE), H5S_SCALAR);
+
+        hsize_t len = 10;
+        H5::DataSpace dspace(1, &len);
+        dhandle.createAttribute("whee", H5::StrType(0, 10), dspace);
+    }
+
+    H5::H5File handle(path, H5F_ACC_RDONLY);
+    auto dhandle = handle.openDataSet("foobar");
+    auto ahandle = dhandle.openAttribute("ouch");
+    ritsuko::hdf5::check_string_missing_placeholder_attribute(ahandle);
+    expect_string_error("string datatype", dhandle.openAttribute("pain"));
+    expect_string_error("NULL pointer", dhandle.openAttribute("stuff"));
+    expect_string_error("scalar", dhandle.openAttribute("whee"));
 }
