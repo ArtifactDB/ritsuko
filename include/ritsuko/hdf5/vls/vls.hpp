a769334cdf18240866ce532198d906ecf5ced695
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-26 20:37:09 -0800

    Nicer docs.

--- include/ritsuko/hdf5/vls/vls.hpp
+++ include/ritsuko/hdf5/vls/vls.hpp
@@ -27,16 +27,21 @@ namespace hdf5 {
  * This is defined by two HDF5 datasets - one storing a VLS heap, and another storing pointers into that heap.
  *
  * - The VLS heap is a 1-dimensional dataset of unsigned 8-bit integers, containing the concatenation of all variable length strings in the array.
- *   This is decoded to a string by casting the integers to `unsigned char` and then reading them via an aliased `char *`.
+ *   This is decoded by casting the integers to `unsigned char` and then reading them via an aliased `char *`.
  * - The pointer dataset is an \f$N\f$-dimensional dataset of a compound datatype defined by `define_pointer_datatype()`.
- *   Each entry of the pointer dataset contains the starting offset and length of a single string on the heap. 
+ *   Each entry of the pointer dataset contains the starting offset and length of a single VLS on the heap. 
  *
  * The idea is to read the pointer dataset into an array of `Pointer` instances,
  * and then use the offset and length for each `Pointer` to extract a slice of characters from the heap.
- * Each slice defines a string, possibly of length shorter than the slice if an intervening null terminator is present.
- * Each slice should lie within the range of the heap; otherwise, there are no restrictions.
- * Slices for consecutive `Pointer`s do not have to be ordered or contiguous,
- * and different `Pointer`s can even refer to the same or overlapping slices (e.g., to improve compression for repeated strings).
+ * Each slice defines a VLS without any need for a null terminator.
+ * The only restriction on the slice is that it should lie within range of the heap - otherwise, anything is allowed:
+ *
+ * - Slices for consecutive `Pointer`s do not have to be ordered or contiguous.
+ *   This allows one or more entries in the `Pointer` dataset to be modified without invalidating other entries.
+ * - Different `Pointer`s can refer to the same or even overlapping slices.
+ *   This provides some opportunities to improve compression for repeated strings.
+ * - If a null terminator is present in the slice, it should terminate the string before the end of the slice.
+ *   This allows the slice to be reused for shorter strings when modifying a single VLS.
  * 
  * Typically the pointer and heap datasets for a single VLS array will be stored in their own group,
  * where they can be opened by `open_pointers()` and `open_heap()`, respectively.
