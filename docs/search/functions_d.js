3dc6d44a940d1ee1a5f87938360632878ff1f163
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-09-18 06:22:42 +0000

    Deploying to gh-pages from @ ArtifactDB/ritsuko@9da8ded63aa7520e14de2397ac1c577b52340a6a 🚀

--- docs/search/functions_d.js
+++ docs/search/functions_d.js
@@ -1,13 +1,11 @@
 var searchData=
 [
   ['validate_5f1d_5fpointers_0',['validate_1d_pointers',['../namespaceritsuko_1_1cvls.html#a4bb581e32522b443a81715d9cd9667d0',1,'ritsuko::cvls']]],
-  ['validate_5f1d_5fstring_5fattribute_1',['validate_1d_string_attribute',['../namespaceritsuko_1_1hdf5.html#a99c585a1063e05eec5385db435d1dfd1',1,'ritsuko::hdf5']]],
-  ['validate_5f1d_5fstrings_2',['validate_1d_strings',['../namespaceritsuko_1_1hdf5.html#ab34d95856e364d1d26539a100b360576',1,'ritsuko::hdf5']]],
-  ['validate_5fheap_3',['validate_heap',['../namespaceritsuko_1_1cvls.html#abc6dac5a614dbea828e079da7f2265ad',1,'ritsuko::cvls']]],
-  ['validate_5fnd_5fpointers_4',['validate_nd_pointers',['../namespaceritsuko_1_1cvls.html#a8575dc2c8283fd17c3439e5ebe2c47ea',1,'ritsuko::cvls']]],
-  ['validate_5fnd_5fstrings_5',['validate_nd_strings',['../namespaceritsuko_1_1hdf5.html#ae3110b30fa3ea0fcd8a775ced2f66828',1,'ritsuko::hdf5']]],
-  ['validate_5fpointer_5fdatatype_6',['validate_pointer_datatype',['../namespaceritsuko_1_1cvls.html#a644674df96b20adb0dda470e12c54496',1,'ritsuko::cvls::validate_pointer_datatype(const H5::CompType &amp;type, const std::size_t offset_precision, const std::size_t length_precision)'],['../namespaceritsuko_1_1cvls.html#a836e42b0743f9a3c576d54749cac04fa',1,'ritsuko::cvls::validate_pointer_datatype(const H5::DataSet &amp;data, const std::size_t offset_precision, const std::size_t length_precision)']]],
-  ['validate_5fscalar_5fpointer_7',['validate_scalar_pointer',['../namespaceritsuko_1_1cvls.html#a85ae1bea23d03cefafe86ec507e19d84',1,'ritsuko::cvls']]],
-  ['validate_5fscalar_5fstring_8',['validate_scalar_string',['../namespaceritsuko_1_1hdf5.html#ae5215e0cac9f009ec0c09d8e59794004',1,'ritsuko::hdf5']]],
-  ['validate_5fscalar_5fstring_5fattribute_9',['validate_scalar_string_attribute',['../namespaceritsuko_1_1hdf5.html#a93db4d46fb9c99c155d179f5ba3ca820',1,'ritsuko::hdf5']]]
+  ['validate_5f1d_5fstrings_1',['validate_1d_strings',['../namespaceritsuko_1_1hdf5.html#ab34d95856e364d1d26539a100b360576',1,'ritsuko::hdf5::validate_1d_strings(const H5::DataSet &amp;data, hsize_t full_length)'],['../namespaceritsuko_1_1hdf5.html#a651c0e80805d28cda82d65f20c9677b5',1,'ritsuko::hdf5::validate_1d_strings(const H5::Attribute &amp;attr, hsize_t full_length)']]],
+  ['validate_5fheap_2',['validate_heap',['../namespaceritsuko_1_1cvls.html#abc6dac5a614dbea828e079da7f2265ad',1,'ritsuko::cvls']]],
+  ['validate_5fnd_5fpointers_3',['validate_nd_pointers',['../namespaceritsuko_1_1cvls.html#a8575dc2c8283fd17c3439e5ebe2c47ea',1,'ritsuko::cvls']]],
+  ['validate_5fnd_5fstrings_4',['validate_nd_strings',['../namespaceritsuko_1_1hdf5.html#ae3110b30fa3ea0fcd8a775ced2f66828',1,'ritsuko::hdf5']]],
+  ['validate_5fpointer_5fdatatype_5',['validate_pointer_datatype',['../namespaceritsuko_1_1cvls.html#a644674df96b20adb0dda470e12c54496',1,'ritsuko::cvls::validate_pointer_datatype(const H5::CompType &amp;type, const std::size_t offset_precision, const std::size_t length_precision)'],['../namespaceritsuko_1_1cvls.html#a836e42b0743f9a3c576d54749cac04fa',1,'ritsuko::cvls::validate_pointer_datatype(const H5::DataSet &amp;data, const std::size_t offset_precision, const std::size_t length_precision)']]],
+  ['validate_5fscalar_5fpointer_6',['validate_scalar_pointer',['../namespaceritsuko_1_1cvls.html#a85ae1bea23d03cefafe86ec507e19d84',1,'ritsuko::cvls']]],
+  ['validate_5fscalar_5fstring_7',['validate_scalar_string',['../namespaceritsuko_1_1hdf5.html#ae5215e0cac9f009ec0c09d8e59794004',1,'ritsuko::hdf5::validate_scalar_string(const H5::DataSet &amp;data)'],['../namespaceritsuko_1_1hdf5.html#aea6a31e2a14d430bed3bb5bf0673dfcc',1,'ritsuko::hdf5::validate_scalar_string(const H5::Attribute &amp;attr)']]]
 ];
